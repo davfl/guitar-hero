45b45113fc43dabeaa6fbcb5ffdc704525e730c3
Agyei Nyame <florinidavide@istitutocorni.it>
2024-01-01 11:46:57 +0100

    aggiunta grafica

---
+++ src/GesctisciPallini.java
@@ -0,0 +1,35 @@
+
+import java.awt.Label;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAVIDE
+ */
+/*public class GesctisciPallini extends Thread {
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private Label label4;
+    private Label label5;
+    private long count;
+
+    public GesctisciPallini(Label label1, Label label2, Label label3, Label label4, Label label5) {
+        this.label1 = label1;
+        this.label2 = label2;
+        this.label3 = label3;
+        this.label4 = label4;
+        this.label5 = label5;
+        count=0;
+    }
+  
+    public void run(){
+        
+    }
+    
+}
+*/
